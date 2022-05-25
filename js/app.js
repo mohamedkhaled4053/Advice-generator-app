0bda15cf4145a6e63158c97de98cb9163fe45b77
mohamedkhaled4053 <mohamedkhaled4053@gmail.com>
2022-05-25 10:23:31 +0200

    add JS code

---
+++ js/app.js
@@ -0,0 +1,18 @@
+let dice = document.querySelector('.dice')
+let h1 = document.querySelector('h1')
+let p = document.querySelector('p')
+
+
+dice.onclick = getAdvice
+
+function getAdvice(){
+    fetch('https://api.adviceslip.com/advice')
+    .then(res => res.json())
+    .then(insertAdvice)
+}
+
+function insertAdvice(res) {
+    h1.textContent = `advice #${res.slip.id}`
+    p.textContent = res.slip.advice
+    console.log(res);
+}
